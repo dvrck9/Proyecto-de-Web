e4277c93b1790a3533678382a8abb66ce3c118c3
CharlyHilasaca <charly.hilasaca@tecsup.edu.pe>
2025-07-05 00:31:09 -0500

    enrutamiento

--- admin_restaurant/urls.py
+++ admin_restaurant/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('restaurantes.urls')),
+    path('', include('restaurantes.urls')),
 ]
